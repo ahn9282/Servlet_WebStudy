fd9b1df86ab7df380f5045923110ac2d9b2d56de
tjqwn <tjqwn@ahn-laptop>
2024-02-20 19:14:21 +0900

    std

--- jsp_database/src/edu/sejong/ex/beanEx/BeanDto.java
+++ jsp_database/src/edu/sejong/ex/beanEx/BeanDto.java
@@ -1,84 +1,104 @@
 package edu.sejong.ex.beanEx;
 
 public class BeanDto {
-private String empno;
-private String ename;
-private String job;
-private String mgr;
-private String hiredate;
-private String sal;
-private String comm;
-private String deptno;
+	private String empno;
+	private String ename;
+	private String job;
+	private String mgr;
+	private String hiredate;
+	private String sal;
+	private String comm;
+	private String deptno;
 
-public BeanDto() {
-	
-}
-public BeanDto(String empno,String ename,String job,String mgr,String hiredate,String sal,String comm,String deptno) {
-	
-}
+	public BeanDto() {
 
-public String getEmpno() {
-	return empno;
-}
+	}
 
-public void setEmpno(String empno) {
-	this.empno = empno;
-}
+	public BeanDto(String empno, String ename, String job, String mgr, String hiredate, String sal, String comm,
+			String deptno) {
+		this.ename = ename;
+		this.empno=empno;
+		this.job=job;
+		this.mgr=mgr;
+		this.hiredate=hiredate;
+		this.sal=sal;
+		this.comm=comm;
+		this.deptno=deptno;
 
-public String getEname() {
-	return ename;
-}
+	}
 
-public void setEname(String ename) {
-	this.ename = ename;
-}
 
-public String getJob() {
-	return job;
-}
+	public String getEmpno() {
+		return empno;
+	}
 
-public void setJob(String job) {
-	this.job = job;
-}
+	public void setEmpno(String empno) {
+		this.empno = empno;
+	}
 
-public String getMgr() {
-	return mgr;
-}
+	public String getEname() {
+		return ename;
+	}
 
-public void setMgr(String mgr) {
-	this.mgr = mgr;
-}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
 
-public String getHiredate() {
-	return hiredate;
-}
+	public String getJob() {
+		return job;
+	}
 
-public void setHiredate(String hiredate) {
-	this.hiredate = hiredate;
-}
+	public void setJob(String job) {
+		this.job = job;
+	}
 
-public String getSal() {
-	return sal;
-}
+	public String getMgr() {
+		return mgr;
+	}
 
-public void setSal(String sal) {
-	this.sal = sal;
-}
+	public void setMgr(String mgr) {
+		this.mgr = mgr;
+	}
 
-public String getComm() {
-	return comm;
-}
+	public String getHiredate() {
+		return hiredate;
+	}
 
-public void setComm(String comm) {
-	this.comm = comm;
-}
+	public void setHiredate(String hiredate) {
+		this.hiredate = hiredate;
+	}
 
-public String getDeptno() {
-	return deptno;
-}
+	public String getSal() {
+		return sal;
+	}
 
-public void setDeptno(String deptno) {
-	this.deptno = deptno;
-}
+	public void setSal(String sal) {
+		this.sal = sal;
+	}
+
+	public String getComm() {
+		return comm;
+	}
+
+	public void setComm(String comm) {
+		this.comm = comm;
+	}
+
+	public String getDeptno() {
+		return deptno;
+	}
+
+	public void setDeptno(String deptno) {
+		this.deptno = deptno;
+	}
+	@Override
+	public String toString() {
+		return "부서번호 :  " +this.getDeptno()+ 
+				" || 사원 이름 : " +this.getEname()+ 
+				" || 직업 : " +this.getJob()+ 
+				" || 커미션 : " +this.getComm()+ 
+				" || 매니저 : " +this.getMgr()+ 
+				" || 사원번호 : " +this.getEmpno()+ "<br>";
+	}
 
 }
--- jsp_database/src/edu/sejong/ex/beanEx/beanDao.java
+++ jsp_database/src/edu/sejong/ex/beanEx/beanDao.java
@@ -1,5 +1,85 @@
 package edu.sejong.ex.beanEx;
 
-public class beanDao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import edu.sejong.ex.empD.EmpDto;
+
+public class beanDao {
+	String driver = "oracle.jdbc.driver.OracleDriver";
+	String url = "jdbc:oracle:thin:@//localhost:1521/xe";
+	String id = "scott";
+	String pw = "tiger";
+	ArrayList<BeanDto> emplist = new ArrayList<>();
+
+	public beanDao() {
+		try {
+			Class.forName(driver);
+		} catch (Exception e) {
+		}
+	}
+
+	public void setList(String sql) {
+
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, id, pw);
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				String empno = rs.getString("empno");
+				String ename = rs.getString("ename");
+				String job = rs.getString("job");
+				String mgr = rs.getString("mgr");
+				String hiredate = rs.getString("hiredate");
+				String deptno = rs.getString("deptno");
+				String sal = rs.getString("sal");
+				String comm = rs.getString("comm");
+
+				BeanDto empdto = new BeanDto(empno, ename, job, mgr, deptno, hiredate, sal, comm);
+				emplist.add(empdto);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		} finally {
+			try {
+
+				if (rs != null)
+					rs.close();
+
+				if (stmt != null)
+					stmt.close();
+
+				if (con != null)
+					con.close();
+			} catch (Exception e2) {
+				// TODO: handle exception
+			}
+		}
+	}
+
+	public String getList() {
+		String str = "";
+		for (BeanDto b : emplist) {
+			str +=" 사원 번호 : " + b.getEmpno() + 
+					" 사원 이름 : " + b.getEname() + 
+					" 직업 : " + b.getJob() +
+					"급여 : " + b.getSal() + 
+					" 입사일 : " + b.getHiredate() +
+					" 커미션 : " + b.getComm() +
+					" 매니저 : " + b.getMgr() + 
+					" 부서 번호 : " + b.getDeptno() + "<br>";
+					
+		}
+
+		return str;
+	}
 }
